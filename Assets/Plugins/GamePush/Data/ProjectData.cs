5c9463ee3dabf2696560967e080ec192180084fb
alexandrov-vasiliy <zowel.vasili@yandex.ru>
2024-07-25 17:48:55 +0300

    game push

---
+++ Assets/Plugins/GamePush/Data/ProjectData.cs
@@ -0,0 +1,9 @@
+namespace GamePush.Data
+{
+    public static class ProjectData
+    {
+        public static string ID = "15912";
+        public static string TOKEN = "VJwtkgphKKb5rOv8lzlQvDoGHYgxbkoC";
+        public static int GAMEREADY_AUTOCALL = 0;
+    }
+}
---
+++ Assets/Plugins/GamePush/JS/_dataFields.jspre
@@ -0,0 +1,4 @@
+const dataProjectId = '15912';
+const dataPublicToken = 'VJwtkgphKKb5rOv8lzlQvDoGHYgxbkoC';
+const showPreloaderAd = 'False';
+const autocallGameReady = '0';
